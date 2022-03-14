c7db8eac46b615c9cc05bfeda38bb018d3085697
Jon Clarke <jonclarke1984@googlemail.com>
2022-03-14 11:09:18 +0000

    DiaryEntry tests passed

--- Design A Multi-Class System/lib/multiclass_DiaryEntry.rb
+++ Design A Multi-Class System/lib/multiclass_DiaryEntry.rb
@@ -1,9 +1,31 @@
 class DiaryEntry
-  def intialize(title, contents) # title, contents are strings
-  # ...
+  def initialize(title, contents) # title, contents are strings
+
+    fail "title and contents are blank" if title == "" && contents == ""
+    fail "title is blank" if title == ""
+    fail "contents are blank" if contents == ""
+  
+    @title = title
+    @contents = contents
+  end
+
+  def title
+    @title
+  end
+
+  def contents
+   @contents
   end
 
   def read_entry
-  # returns formatted diary entry
+    "Title: #{@title}. Entry: #{@contents}\n"    
   end
+
+  # def count_words
+  #   @contents.split.count
+  # end
+
+  # def reading_time(wpm) 
+  #   count_words / wpm
+  # end
 end
