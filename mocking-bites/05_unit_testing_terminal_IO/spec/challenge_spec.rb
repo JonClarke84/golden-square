e96760ac2254e0d3182d795bc55e9b33973146f0
Jon Clarke <jonclarke1984@googlemail.com>
2022-03-15 17:02:18 +0000

    challenge done

---
+++ mocking-bites/05_unit_testing_terminal_IO/spec/challenge_spec.rb
@@ -0,0 +1,18 @@
+require 'challenge'
+
+RSpec.describe StringRepeater do
+  it 'repeats a given string a given number of times' do
+    io = double :io
+
+    expect(io).to receive(:puts).with("Hello. I will repeat a string many times.")
+    expect(io).to receive(:puts).with("Please enter a string")
+    expect(io).to receive(:gets).and_return("TWIX")
+    expect(io).to receive(:puts).with("Please enter a number of repeats")
+    expect(io).to receive(:gets).and_return("10")
+    expect(io).to receive(:puts).with("Here is your result:")
+    expect(io).to receive(:puts).with("TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX")
+
+    string_repeater = StringRepeater.new(io)
+string_repeater.run
+  end
+end
