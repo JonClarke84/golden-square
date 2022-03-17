a5e25068495d8f611e8876084ec57843b6436c12
Jon Clarke <jonclarke1984@googlemail.com>
2022-03-17 09:22:26 +0000

    env files set

--- solo_project/lib/send_sms.rb
+++ solo_project/lib/send_sms.rb
@@ -1,8 +1,9 @@
 # Download the twilio-ruby library from twilio.com/docs/libraries/ruby
 require 'twilio-ruby'
+require 'dotenv/load'
 
-account_sid = 'AC029da4ff5f230b0498264fd36783100a'
-auth_token = 'cea9e3818ea880f0a245c08c909eb5d2'
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '+447360542191' # Your Twilio number
